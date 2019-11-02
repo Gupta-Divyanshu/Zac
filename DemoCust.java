e538bab539502b970b881b554a560a70549bd6b2
Gupta-Divyanshu <divyanshu.gupta@lntinfotech.com>
2019-11-02 02:15:42 -0700

    Update DemoCust.java

--- DemoCust.java
+++ DemoCust.java
@@ -3,11 +3,13 @@ class DemoCust
     int CustID;
     
     String CName;
-        
-        public int getCustID(){
+     
+    public int getCustID()
+    {
          return CustID;
-        }
-    public String getCustName(){
-    return CName;
      }
+     public String getCustName()
+      {
+          return CName;
+      }
 }
