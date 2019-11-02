0e5c5302dc41101fb8ceba3262d8b35ee4ecf019
Gupta-Divyanshu <divyanshu.gupta@lntinfotech.com>
2019-11-02 02:14:47 -0700

    Update DemoCust.java
    added getter method

--- DemoCust.java
+++ DemoCust.java
@@ -1,9 +1,13 @@
 class DemoCust
 {
-    int CustID,
-    String CName
+    int CustID;
+    
+    String CName;
         
         public int getCustID(){
          return CustID;
         }
+    public String getCustName(){
+    return CName;
+     }
 }
